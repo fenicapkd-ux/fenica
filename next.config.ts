ed4afc3e975e293768f843047255413f55addb66
Daniel <phuongdongiot@gmail.com>
2026-03-15 14:59:10 +0700

    update: comporess build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compress: true,
+  reactStrictMode: true,
+  experimental: {
+    viewTransition: true,
+  },
 };
 
 export default nextConfig;
